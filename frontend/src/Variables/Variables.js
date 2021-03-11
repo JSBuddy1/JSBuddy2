e09bc43070a4a6a6e553d5bf1cfa064145923055
TalkToVictor <itsvictornwani@yahoo.com>
2021-03-11 09:32:35 -0500

    victor

--- frontend/src/Variables/Variables.js
+++ frontend/src/Variables/Variables.js
@@ -1,44 +1,44 @@
-import React,{useState} from 'react';
-import {slides} from "./VariableSlides";
+import React, { useState } from 'react';
+import { slides } from "./VariableSlides";
 import Playground from "../components/CodeEditor/Playground";
-import {describe, it, expect, run} from "jest-lite";
+import { describe, it, expect, run } from "jest-lite";
 
 console.log("hello")
 function Variables(props) {
 
-    let [page, setPage] = useState(props.match.params.page);
-    let [code, setCode] = useState("");
-  
-    const switchPage = () => {
-      setPage(Math.min(9, ++page));
-      props.history.push(String(page));
-    };
-  
-    const prevPage = () => {
-      setPage(Math.max(1, --page));
-      props.history.push(String(page));
-    };
+  let [page, setPage] = useState(props.match.params.page);
+  let [code, setCode] = useState("");
 
-    async function doesItPass(str) {
-        console.log(str, typeof str);
-        const obj = eval(str);
-        //const sum = new String("return " + str)(); //This is the part that needs to be better understood.
-        console.log(obj, typeof obj);
-        describe("obj", () => slides[page]?.test(obj));
-        const result = await run();
-        console.log(result);
-        let lastResult = result.pop();
-        if (lastResult.status == "pass") {
-          alert("pass");
-          props.history.push(String(++page));
-        } else {
-          alert("try again");
-        }
-      }
+  const switchPage = () => {
+    setPage(Math.min(9, ++page));
+    props.history.push(String(page));
+  };
 
-    return (
-        <div>
-        <h3>{slides[page]?.text}</h3>
+  const prevPage = () => {
+    setPage(Math.max(1, --page));
+    props.history.push(String(page));
+  };
+
+  async function doesItPass(str) {
+    console.log(str, typeof str);
+    const val = eval(str);
+    //const sum = new String("return " + str)(); //This is the part that needs to be better understood.
+    console.log(val, typeof val);
+    describe("val", () => slides[page]?.test(val));
+    const result = await run();
+    console.log(result);
+    let lastResult = result.pop();
+    if (lastResult.status == "pass") {
+      alert("pass");
+      props.history.push(String(++page));
+    } else {
+      alert("try again");
+    }
+  }
+
+  return (
+    <div>
+      <h3>{slides[page]?.text}</h3>
       <div dangerouslySetInnerHTML={{ __html: slides[page]?.html }}></div>
       <br />
       {slides[page]?.playground ? (
@@ -52,9 +52,9 @@ function Variables(props) {
       <span></span>
       {props.match.params.page} {page} <span></span>
       <button onClick={switchPage}>Next</button>
-            
-        </div>
-    );
+
+    </div>
+  );
 }
 
 export default Variables;
